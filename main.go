1de037ac9529205354fbf0c62cea71a66fa315c1
Jhamil Antony Crispin Martel <wild@MacBook-Pro-de-Jhamil.local>
2025-08-16 21:16:43 -0500

    creacion de tablass

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"net/http"
 
 	"github.com/faztweb/go-gorm-restapi/db"
+	"github.com/faztweb/go-gorm-restapi/models"
 	"github.com/faztweb/go-gorm-restapi/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	db.DBConnection()
+
+	db.DB.AutoMigrate(models.User{}, models.Task{})
 
 	r := mux.NewRouter()
 
